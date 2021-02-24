94065fd704acfedd1e4650582348f2c7b3e66a88
C7Knox <knox@uleth.ca>
2021-02-23 21:04:57 -0700

    CSS change added

--- js/content.js
+++ js/content.js
@@ -1,7 +1,18 @@
-console.log("Extension is running! :)")
+console.log("Extension is running! :)");
 
-// TRY: updating the text content of an element or set of elements on one or more pages
 
-// TRY: adding a click event listener to an element that changes the background of that element to a new color
 
-// TRY: using a mouseenter event listener to update text to something new.
+
+//was trying to go through jQuery to access all the images before I realized I could just use CSS
+//var $images = [];
+//var elements = document.body.getElementsByTagName("*");
+//Array.prototype.forEach.call( elements, function (el) {
+//    var style = window.getComputedStyle(el, false);
+//    if ( style.baclgroundImage != "none" ) {
+//        $images.push(style.backgroundImage.slice(4, -1).replace(/['"]/g, ""))
+//    }
+//})
+//$images.addClass("bright");
+
+//Credit\\
+//Code inspired and borrowed from https://stackoverflow.com/questions/52444784/detect-all-images-with-javascript-in-an-html-page/52721409
